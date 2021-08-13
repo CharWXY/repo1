c7e4d1cd89ed4806b70cb99090193e7a3272ffe3
zhangsan <zhangsan@163.com>
2021-08-13 16:59:30 +0800

    把git-test工程添加到本地仓库

---
+++ git_test/src/main/java/com/itheima/first/first.java
@@ -0,0 +1,10 @@
+package com.itheima.first;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("hhhhhhhhhhhh");
+        System.out.println("hhhhhhhhhhhh");
+        System.out.println("hhhhhhhhhhhh");
+        System.out.println("hhhhhhhhhhhh");
+    }
+}
